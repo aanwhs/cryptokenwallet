d22e56909150c69233cf955d88b8469f7732629c
Kang Aan Wasi <43213343+aanwhs@users.noreply.github.com>
2023-05-26 21:05:12 +0700

    Create page.js

---
+++ assets/js/page.js
@@ -0,0 +1,30 @@
+function Home_Page() {
+	location.replace('//aanwhs.github.io/cryptokenwallet')
+}
+function CrypTokenWallet_Page() {
+	location.replace('//aanwhs.github.io/cryptokenwallet.html')
+}
+function About_Page() {
+	location.replace('//aanwhs.github.io/about.html')
+}
+function Disclaimer_Page() {
+	location.replace('//aanwhs.github.io/disclaimer.html')
+}
+function PrivacyPolicy_Page() {
+	location.replace('//aanwhs.github.io/privacy-policy.html')
+}
+function CookiePolicy_Page() {
+	location.replace('//aanwhs.github.io/cookie-policy.html')
+}
+function Wallet_Page() {
+	location.replace('//aanwhs.github.io/wallet.html')
+}
+function GenerateWalletAddress_Page() {
+	location.replace('//aanwhs.github.io/generate-wallet-address.html')
+}
+function CheckWalletAddress_Page() {
+	location.replace('//aanwhs.github.io/check-wallet-address.html')
+}
+function ConnectToWallet_Page() {
+	location.replace('//aanwhs.github.io/connect-to-wallet.html')
+}
