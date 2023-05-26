56ff2efa31e16f58183ce82dfd22ac937e0d8006
Kang Aan Wasi <43213343+aanwhs@users.noreply.github.com>
2023-05-26 21:38:06 +0700

    Update page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -5,26 +5,26 @@ function CrypTokenWallet_Page() {
 	location.replace('//aanwhs.github.io/cryptokenwallet.html')
 }
 function About_Page() {
-	location.replace('//aanwhs.github.io/about.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/about.html')
 }
 function Disclaimer_Page() {
-	location.replace('//aanwhs.github.io/disclaimer.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/disclaimer.html')
 }
 function PrivacyPolicy_Page() {
-	location.replace('//aanwhs.github.io/privacy-policy.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/privacy-policy.html')
 }
 function CookiePolicy_Page() {
-	location.replace('//aanwhs.github.io/cookie-policy.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/cookie-policy.html')
 }
 function Wallet_Page() {
-	location.replace('//aanwhs.github.io/wallet.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/wallet.html')
 }
 function GenerateWalletAddress_Page() {
-	location.replace('//aanwhs.github.io/generate-wallet-address.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/generate-wallet-address.html')
 }
 function CheckWalletAddress_Page() {
-	location.replace('//aanwhs.github.io/check-wallet-address.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/check-wallet-address.html')
 }
 function ConnectToWallet_Page() {
-	location.replace('//aanwhs.github.io/connect-to-wallet.html')
+	location.replace('//aanwhs.github.io/cryptokenwallet/connect-to-wallet.html')
 }
